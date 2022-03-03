176e77d49caf81cd34e48287572d2fdadbc9a896
Bald Badger <32609230+Bald-Badger@users.noreply.github.com>
2022-03-03 16:13:42 -0500

    f

--- fbputchar.c
+++ fbputchar.c
@@ -114,6 +114,17 @@ void fbputs(const char *s, int row, int col)
 	while ((c = *s++) != 0) fbputchar(c, row, col++);
 }
 
+/*
+clears the frame buffer
+*/
+void fbclear() {
+	for (int row = 0; row < ROWS; row++) {
+		for (int col = 0; col < COLS; col++) {
+			fbputs("", row, col);
+		}
+	}
+}
+
 /* 8 X 16 console font from /lib/kbd/consolefonts/lat0-16.psfu.gz
 
 od --address-radix=n --width=16 -v -t x1 -j 4 -N 2048 lat0-16.psfu

1107cfe1a6f0ed066e5a894e4e91aa2eefccd46e
Bald Badger <32609230+Bald-Badger@users.noreply.github.com>
2022-03-03 23:02:53 -0500

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -295,15 +295,15 @@ void *input_thread_f(void *ignored) {
 				if (cursor < message_ptr) {
 					if (message_ptr / COLS == 0) {
 						if (message_ptr % COLS != 0) {
-							screen[CURSER_L1][message_ptr % COLS + 1] = ASCII_UNDERSCORE;
+							screen[CURSER_L1][cursor % COLS + 1] = ASCII_UNDERSCORE;
 						}
-						screen[CURSER_L1][message_ptr % COLS] = ASCII_SPACE;
+						screen[CURSER_L1][cursor % COLS] = ASCII_SPACE;
 					} 
 					else {
 						if (message_ptr % COLS != 0) {
-							screen[CURSER_L2][message_ptr % COLS + 1] = ASCII_UNDERSCORE;
+							screen[CURSER_L2][cursor % COLS + 1] = ASCII_UNDERSCORE;
 						}
-						screen[CURSER_L2][message_ptr % COLS] = ASCII_SPACE;
+						screen[CURSER_L2][cursor % COLS] = ASCII_SPACE;
 					}
 					cursor ++;
 				}
@@ -313,14 +313,13 @@ void *input_thread_f(void *ignored) {
 			else if (packet.keycode[0] == KEY_LEFT) {
 				if (cursor > 0) {
 					if (message_ptr / COLS == 0) {
-						screen[CURSER_L1][message_ptr % COLS - 1] = ASCII_UNDERSCORE;
-						screen[CURSER_L1][message_ptr % COLS] = ASCII_SPACE;
-						printf("WTF\n");
+						screen[CURSER_L1][cursor % COLS - 1] = ASCII_UNDERSCORE;
+						screen[CURSER_L1][cursor % COLS] = ASCII_SPACE;
 					} else {
-						screen[CURSER_L2][message_ptr % COLS - 1] = ASCII_UNDERSCORE;
-						screen[CURSER_L2][message_ptr % COLS] = ASCII_SPACE;
+						screen[CURSER_L2][cursor % COLS - 1] = ASCII_UNDERSCORE;
+						screen[CURSER_L2][cursor % COLS] = ASCII_SPACE;
 					}
-					cursor --;
+					cursor -= 1;
 				}
 				refresh();
 			}

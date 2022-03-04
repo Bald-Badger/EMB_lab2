a313521a3bee05662a8d945d6c1ff1b05f7cf207
Bald Badger <32609230+Bald-Badger@users.noreply.github.com>
2022-03-03 23:28:40 -0500

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -333,7 +333,7 @@ void *input_thread_f(void *ignored) {
 					if (cursor < message_ptr) {
 						memmove(&message[cursor+1], &message[cursor], BUFFER_SIZE);
 					}
-					if ((cursor / COLS) == 0) {
+					if ((message_ptr / COLS) == 0) {
 						screen[USER_INPUT_L1][cursor % COLS] = key;
 						screen[CURSER_L1][cursor % COLS] = ASCII_UNDERSCORE;
 						if (message_ptr % COLS > 0) {
@@ -347,8 +347,8 @@ void *input_thread_f(void *ignored) {
 						}
 					}
 
-					if (cursor >= (COLS*2 - 1)) {
-						//message_ptr = message_ptr - COLS;
+					if (message_ptr >= (COLS*2 - 1)) {
+						message_ptr = message_ptr - COLS;
 						shift_user();
 					}
 
